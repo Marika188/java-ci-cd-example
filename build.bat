ceb4ba5345360efc163b734feb7129c737affc21
Marika188 <rylev.artemy@gmail.com>
2024-12-16 23:30:41 +0300

    Fix missing AppTest.java file or update build step

--- build.bat
+++ build.bat
@@ -1,20 +1,11 @@
 @echo off
+rem Устанавливаем путь к JUnit JAR
+set JUNIT_PATH=lib/junit-platform-console-standalone-1.10.0.jar
 
-rem Установим переменные для папок
-set LIB_PATH=lib\junit-platform-console-standalone-1.10.0.jar
-set SRC_PATH=src\main\java
-set TEST_PATH=src\test\java
-set BUILD_PATH=out
+rem Компилируем основной код и тесты
+javac -cp ".;%JUNIT_PATH%" src/main/java/com/example/App.java src/test/java/com/example/AppTest.java
 
-rem Удалим старую сборку (если есть)
-if exist %BUILD_PATH% rmdir /s /q %BUILD_PATH%
-mkdir %BUILD_PATH%
+rem Запускаем тесты с использованием JUnit
+java -cp ".;%JUNIT_PATH%" org.junit.runner.JUnitCore com.example.AppTest
 
-rem Компиляция кода
-echo Компиляция исходного кода...
-javac -d %BUILD_PATH% -cp %LIB_PATH% %SRC_PATH%\com\example\App.java %TEST_PATH%\com\example\AppTest.java
-
-rem Запуск тестов
-echo Запуск тестов...
-java -jar %LIB_PATH% -cp %BUILD_PATH% --scan-classpath --include-classname=com.example.AppTest
 
